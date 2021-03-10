296fcca9c65cf9d9a8ab9e6d91b9db98587209f4
yusuke furukawa <yusuke.furukawa@chatwork.com>
2021-03-10 14:53:29 +0900

    solve level1

--- src/main/scala/MessageCountActorApp.scala
+++ src/main/scala/MessageCountActorApp.scala
@@ -1,3 +1,24 @@
-object MessageCountActorApp extends App {
+import akka.actor.{Actor, ActorSystem, Props}
 
+class MyActor extends Actor {
+  var count = 0
+
+  def receive = {
+    case _ => {
+      count = count + 1
+      println(count)
+    }
+  }
+}
+
+object MessageCountActorApp extends App {
+  val system = ActorSystem("actorStudy")
+
+  val myActor = system.actorOf(Props[MyActor], "myActor")
+
+  for (i <- 1 to 10000) {
+    myActor ! "Hello!"
+  }
+
+  Thread.currentThread().join()
 }
